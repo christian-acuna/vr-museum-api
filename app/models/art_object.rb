fea547b33f8478e2ceb4244963b0466e1e661792
Christian Acuña <cacuna0828@gmail.com>
2016-12-16 15:06:29 -0800

    test artObject model validations and associations

--- app/models/art_object.rb
+++ app/models/art_object.rb
@@ -1,3 +1,5 @@
 class ArtObject < ActiveRecord::Base
-  
+  validates_presence_of :title, :image_url, :thumbnail_url
+  belongs_to :museum
+  belongs_to :medium
 end
---
+++ spec/models/art_object_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe ArtObject do
+  it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_presence_of(:image_url) }
+  it { is_expected.to validate_presence_of(:thumbnail_url) }
+  it { is_expected.to belong_to(:museum) }
+  it { is_expected.to belong_to(:medium) }
+end
