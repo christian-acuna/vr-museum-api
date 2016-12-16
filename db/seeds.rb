cffc6b06aaec217df2e66f94a82d4ef7b6da7a4b
John Winslow <winslow.johnm@gmail.com>
2016-12-16 12:17:11 -0800

    add json file of 450 getty paintings, seed them into ArtObjects

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+ArtObject.delete_all
+
+require 'json'
+getty = File.read('./db/getty.json')
+data_hash = JSON.parse(getty)
+records = data_hash["Response"]["doc"]["record"]
+
+def getLargePicture(url)
+  url.gsub(/(thumbnail)/, 'enlarge')
+end
+
+def deletePlaceCreated(place)
+  if place == nil
+    nil
+  else
+    place.gsub(/(Place Created: )/, '')
+  end
+end
+
+records.each do |record|
+  ArtObject.create!(
+    museum_id: 1, #1 for everything from Getty?
+    medium_id: 1, #1 for paintings? (these are all paintigs)
+    title: record["PrimaryTitle"],
+    date: record["Date"],
+    artist: record["MakerName"],
+  # no description provided by this API 
+    dimensions: record["Dimensions"],
+    thumbnail_url: record["imageThumbURI"],
+    image_url: getLargePicture(record["imageThumbURI"]),
+    place: deletePlaceCreated(record["Place"]),
+    link_to_object: record["recordLink"],
+  # no credit line provided by this API
+  )
+end
+
+
+
