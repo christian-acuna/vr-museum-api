63d174a15458ff8dc2c2d4cbcffd4c26c0679adf
Christian Acuña <cacuna0828@gmail.com>
2016-12-16 15:30:35 -0800

    create tag model and migration with spec

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161216231111) do
+ActiveRecord::Schema.define(version: 20161216232632) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,12 @@ ActiveRecord::Schema.define(version: 20161216231111) do
     t.string   "name"
     t.string   "location"
     t.string   "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
