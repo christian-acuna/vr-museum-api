53ce74b80afbe522b6f6c00e7d6702170cde3642
parinaz77 <parinaz77@gmail.com>
2016-12-17 14:50:24 -0800

    update routes for collection#index

--- app/serializers/v1/art_object_serializer.rb
+++ app/serializers/v1/art_object_serializer.rb
@@ -1,8 +1,8 @@
 module V1
 	class ArtObjectSerializer < ActiveModel::Serializer
-		attributes :title, :description, :image_url, :thumbnail_url
-		has_one :museum, serializer: V1::MuseumSerializer
-		has_one :medium, serializer: V1::MediumSerializer
+		attributes :title, :description, :image_url, :thumbnail_url, :museum, :medium
+		# has_one :museum, serializer: V1::MuseumSerializer
+		# has_one :medium, serializer: V1::MediumSerializer
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  namespace :v1, defaults: {format: :json} do
+  	resources :collections, only: [:index]
+
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
