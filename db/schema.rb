22ea0762ee75501c0b62b44e619e2fcd4cd0ac42
Christian Acuña <cacuna0828@gmail.com>
2016-12-16 16:04:24 -0800

    create favorite model, migration, and test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161216234340) do
+ActiveRecord::Schema.define(version: 20161217000114) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,16 @@ ActiveRecord::Schema.define(version: 20161216234340) do
     t.integer  "user_id"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "collection_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "collection_id"
   end
 
   create_table "media", force: :cascade do |t|
