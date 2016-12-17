437ea42eacbad8f0bad4e5ab6b0ce18346f3d0b1
parinaz77 <parinaz77@gmail.com>
2016-12-17 15:08:00 -0800

    create relationship between art object and primary art object

--- app/serializers/v1/collection_serializer.rb
+++ app/serializers/v1/collection_serializer.rb
@@ -1,8 +1,10 @@
 module V1
 	class CollectionSerializer < ActiveModel::Serializer
-		attributes :title, :description, :primary_object_id
-		has_many :art_objects, serializer: V1::ArtObjectSerializer
-		has_one :user, serializer: V1::UserSerializer
+		attributes :title, :description, :primary_object
+		# attribute :primary_object_id
+		# has_many :art_objects, serializer: V1::ArtObjectSerializer
+		# has_one :user, serializer: V1::UserSerializer
+		# has_one :primary_object, serializer: V1::PrimaryObjectSerializer
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :v1, defaults: {format: :json} do
   	resources :collections, only: [:index]
-
   end
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
