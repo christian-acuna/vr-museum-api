869f7be5ea37d6d17f1fe6389eb4a9f308692402
parinaz77 <parinaz77@gmail.com>
2016-12-18 16:24:50 -0800

    Add skip before action

--- app/controllers/v1/collections_controller.rb
+++ app/controllers/v1/collections_controller.rb
@@ -1,5 +1,6 @@
 module V1
 	class CollectionsController < ApplicationController
+		skip_before_action :authenticate_user_from_token!
 		def index
 			collections = Collection.all
 			render json: collections, each_serializer: CollectionSerializer
