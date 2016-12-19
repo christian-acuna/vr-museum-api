2773031fec371598e3c9fe5e2a7b785f8a7cb348
Laura Reynolds <lr.rnlds@gmail.com>
2016-12-19 10:33:10 -0800

    Search#search route to include both collections and art_objects

--- app/controllers/v1/search_controller.rb
+++ app/controllers/v1/search_controller.rb
@@ -1,20 +1,23 @@
 module V1
   class SearchController < ApplicationController
   	skip_before_action :authenticate_user_from_token!
-    
+
     def search
       query = params[:q]
 
       if query.blank?
         render status: 400, json: { error: 'Expected parameter `query` '}
       else
-        render(
-          status: 200,
-          json: results_collection = Collection.where(["title LIKE ?", "%#{query}%"]) && ArtObject.where(["title LIKE ?", "%#{query}%"])
-          )
+        # render(
+          # status: 200,
+          results_collection = Collection.where(["title LIKE ?", "%#{query}%"])
+          results_art_objects = ArtObject.where(["title LIKE ?", "%#{query}%"])
+          # json: results_collection = Collection.where(["title LIKE ?", "%#{query}%"]) && ArtObject.where(["title LIKE ?", "%#{query}%"])
+          render json: { collections: results_collection, art_objects: results_art_objects}, status: :ok
+          # )
       end
     end
-    
+
 
   end
 end
