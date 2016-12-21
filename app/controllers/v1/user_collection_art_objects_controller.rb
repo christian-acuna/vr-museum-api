8e67371587eaa26eace7d0e4cd38d836ce5c47a4
Christian Acuña <cacuna0828@gmail.com>
2016-12-21 09:46:54 -0800

    update userCollectionArtObjects

--- app/controllers/v1/user_collection_art_objects_controller.rb
+++ app/controllers/v1/user_collection_art_objects_controller.rb
@@ -2,7 +2,7 @@ module V1
   class UserCollectionArtObjectsController < ApplicationController
 
     def create
-      user = User.find(params[:access_token])
+      user = User.find(params[:user_id])
       p collection = user.collections.find(params[:collection_id])
       p art_object = ArtObject.find(params[:art_object_id])
       p new_art_object= CollectionArtObject.new(collection_id:collection.id,art_object_id:art_object.id)
@@ -11,15 +11,15 @@ module V1
         render( status: 200 )
       else
        render( status: 404 )
-      end 
+      end
     end
 
     def destroy
-      user = User.find(params[:access_token])
+      user = User.find(params[:user_id])
       collection = user.collections.find(params[:collection_id])
       art_object = ArtObject.find(params[:art_object_id])
 
-      delete_object = CollectionArtObject.where(collection_id:collection,art_object_id:art_object)      
+      delete_object = CollectionArtObject.where(collection_id:collection,art_object_id:art_object)
       if CollectionArtObject.delete(delete_object)
         render( status: 200 )
       else
