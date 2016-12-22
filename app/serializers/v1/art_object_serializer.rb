67ab3505bde07eb73e92ea0bd2caffaee5fed4a0
parinaz77 <parinaz77@gmail.com>
2016-12-21 16:10:23 -0800

    add vr_url to serializer

--- app/serializers/v1/art_object_serializer.rb
+++ app/serializers/v1/art_object_serializer.rb
@@ -1,6 +1,6 @@
 module V1
 	class ArtObjectSerializer < ActiveModel::Serializer
-		attributes :id, :title, :description, :image_url, :thumbnail_url, :museum, :medium, :place
+		attributes :id, :title, :description, :image_url, :thumbnail_url, :museum, :medium, :place, :vr_url
 		# has_one :museum, serializer: V1::MuseumSerializer
 		# has_one :medium, serializer: V1::MediumSerializer
 	end
