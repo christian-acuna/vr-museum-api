7e83f692ac32c84ee6c29291b4ed98cb92526d5f
Laura Reynolds <lr.rnlds@gmail.com>
2016-12-22 17:18:58 -0800

    added artist info to serializer

--- app/serializers/v1/art_object_serializer.rb
+++ app/serializers/v1/art_object_serializer.rb
@@ -1,6 +1,6 @@
 module V1
 	class ArtObjectSerializer < ActiveModel::Serializer
-		attributes :id, :title, :description, :image_url, :thumbnail_url, :museum, :medium, :place, :vr_url
+		attributes :id, :title, :description, :image_url, :thumbnail_url, :museum, :medium, :place, :vr_url, :artist, :date, :link_to_object
 		# has_one :museum, serializer: V1::MuseumSerializer
 		# has_one :medium, serializer: V1::MediumSerializer
 	end
