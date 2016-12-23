c652ed8974144346f418679ef4cb1acf6b7b4038
Christian Acuña <cacuna0828@gmail.com>
2016-12-23 10:13:36 -0800

    add vr_mode param to all collections

--- app/controllers/v1/collections_controller.rb
+++ app/controllers/v1/collections_controller.rb
@@ -3,9 +3,12 @@ module V1
 		skip_before_action :authenticate_user_from_token!
 		def index
 			collections = Collection.all
-			render json: collections, each_serializer: CollectionSerializer
+			if params[:vr_mode]
+        render json: collections, each_serializer: CompleteCollectionSerializer
+			else
+				render json: collections, each_serializer: CollectionSerializer
+			end
 		end
 
 	end
-
 end
